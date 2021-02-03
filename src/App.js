a511e4bfa19ce257942a1730ac9c5bbc18403bf1
Juan Urtazun <juan-urtazun@users.noreply.github.com>
2021-02-03 12:13:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import { Navbar } from "./app/Navbar";
+import { AddPostForm } from "./features/posts/AddPostForm";
+import { PostList } from "./features/posts/postsList";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <AddPostForm />
+                <PostList />
+              </>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
